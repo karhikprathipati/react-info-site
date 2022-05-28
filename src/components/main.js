aef129bd4d772deef21388839d027974eab1373d
karthikpratihpati <karthikprathipati1@gmail.com>
2022-05-28 22:32:26 +0530

    created navbar and main content page

---
+++ src/components/main.js
@@ -0,0 +1,8 @@
+function MainComponent(){
+    return (
+        <div>
+            <p>This is the main page for content </p>
+        </div>
+    );
+}
+export default MainComponent;
---
+++ src/components/navbar.js
@@ -0,0 +1,8 @@
+function Navbar(){
+    return (
+        <header>
+        <p>This is the navbar</p>
+        </header>
+    );
+}
+export default Navbar;
