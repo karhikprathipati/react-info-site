4e18558866ece94fabcc6e780e9da9cbdbde985e
karthikpratihpati <karthikprathipati1@gmail.com>
2022-05-28 23:49:23 +0530

    styled the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,12 @@
+import logo from '../reactLogo.png';
+
 function Navbar(){
     return (
-        <header>
-        <p>This is the navbar</p>
-        </header>
+        <nav className="navbar">
+            <img className='element image' src={logo}></img>
+            <h2 className='element'>ReactInfo</h2>
+            <h3 className='element'>React Course - Project 1</h3>
+        </nav>
     );
 }
 export default Navbar;
