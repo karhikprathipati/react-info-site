74a172e6e735b4571271977a4781362598bb2886
karthikpratihpati <karthikprathipati1@gmail.com>
2022-05-29 13:24:14 +0530

    added some changes to navbar and updated the main content

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,14 @@
 function MainComponent(){
     return (
-        <div>
-            <p>This is the main page for content </p>
+        <div className="main">
+            <h1 className="content">Fun Facts about React</h1>
+            <ul className="list">
+                <li className="element">Was released in 2003</li>
+                <li className="element">Was originally created by John Walke</li>
+                <li className="element">Has well over 100k stars</li>
+                <li className="element">is maintened by facebook</li>
+                <li className="element">Powers thousands of apps</li>
+            </ul>
         </div>
     );
 }
